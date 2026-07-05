7537a360aacce2d0cafffcca8e3547b9b45e8b9e
Verswijveren <wout@fited.co>
2026-07-05 09:05:50 +0200

    Vellum canvas recipe alternative

--- obj/Debug/VintageCanvas.AssemblyInfo.cs
+++ obj/Debug/VintageCanvas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VintageCanvas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f18a186d198e6898b5d9a2c5690f260ee4391e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137983b48f089a07a97b6a66559ecccc09c7285c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VintageCanvas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VintageCanvas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
